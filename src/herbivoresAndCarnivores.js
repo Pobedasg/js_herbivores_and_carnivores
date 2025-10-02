5ca3c4837636081060de82628c1a2ec0da94686e
Victoriia <kod.victoriya@gmail.com>
2025-10-02 20:31:07 +0300

    new solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,35 +1,24 @@
 'use strict';
-
 class Animal {
   static alive = [];
   constructor(name, health = 100) {
     this.name = name;
     this.health = health;
 
-    this._aliveRecord = {
-      name: this.name,
-      health: this.health,
-    };
-
     if (this instanceof Herbivore) {
-      this._aliveRecord.hidden = false;
+      this.hidden = false;
     }
-    Animal.alive.push(this._aliveRecord);
+    Animal.alive.push(this);
   }
+
   die() {
-    Animal.alive = Animal.alive.filter((a) => a.health > 0);
+    Animal.alive = Animal.alive.filter((animal) => animal !== this);
   }
 }
 
 class Herbivore extends Animal {
-  constructor(name, health) {
-    super(name, health);
-    this.hidden = false;
-  }
-
   hide() {
     this.hidden = true;
-    this._aliveRecord.hidden = true;
   }
 }
 
@@ -37,7 +26,6 @@ class Carnivore extends Animal {
   bite(herbivore) {
     if (herbivore instanceof Herbivore && !herbivore.hidden) {
       herbivore.health -= 50;
-      herbivore._aliveRecord.health = herbivore.health;
 
       if (herbivore.health <= 0) {
         herbivore.die();
